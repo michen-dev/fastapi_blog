efb964a897dd5322b028a281dea00ec2ad718371
Chenh Hung Minh <horros@Chenhs-MacBook-Air.local>
2026-03-17 21:34:04 +0700

    post request and response

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from schema import PostCreate, PostResponse
 
 
 app = FastAPI()
@@ -36,7 +37,7 @@ def home(request: Request):
 
 
 @app.get("/posts/{post_id}", include_in_schema=False)
-def get_post(request: Request, post_id: int):
+def post_page(request: Request, post_id: int):
     for post in posts:
         if post["id"] == post_id:
             return templates.TemplateResponse(request, "post.html", {"post": post, "title": post["title"][:50]})
@@ -83,6 +84,28 @@ def validation_exception_handler(request: Request, exception: RequestValidationE
     )
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
+
+
+@app.get('api/posts/{post_id}', response_model=PostResponse)
+def get_post():
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.post('/api/posts', response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025"
+    }
+    posts.append(new_post)
+    return new_post
